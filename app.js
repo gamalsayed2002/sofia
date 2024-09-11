551b479edc1fc56a1380b8e545126e2219b84f0d
gamal <gamalsayedccss@gmail.com>
2024-09-11 16:25:50 +0300

    adding send form

--- app.js
+++ app.js
@@ -33,3 +33,28 @@ menu.addEventListener("click", () => {
   activeOne.classList.toggle("active-two");
 });
 new WOW().init();
+// email js
+// emailjs.init("BHPWqZaqxG6tCDcnE");
+emailjs.init("qH1rBr6KP0DO8XSJq");
+const btn = document.getElementById("button");
+
+function sendMail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    name: document.getElementById("name").value,
+    email_id: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    phone: document.getElementById("phone").value,
+  };
+  emailjs.send("service_7h20fkq", "template_a3b4eai", params).then(() => {
+    Swal.fire({
+      title: "تم إرسال البيانات",
+
+      icon: "success",
+    });
+    name = document.getElementById("name").value = "";
+    email = document.getElementById("email").value = "";
+    message = document.getElementById("message").value = "";
+    phone = document.getElementById("phone").value = "";
+  });
+}
